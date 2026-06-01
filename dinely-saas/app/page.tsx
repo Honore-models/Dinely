d569404839e1b79b29e9b6302cdaab43f40a4a9c
honore <honoreniyogushimwa63@gmail.com>
2026-06-01 08:17:48 +0200

    feat(): added image logo

--- dinely-saas/app/page.tsx
+++ dinely-saas/app/page.tsx
@@ -4,6 +4,13 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Image
+        src="/Group 103.png"
+        alt="Vercel Logo"
+        width={100}
+        height={30}
+        priority
+      />
     </div>
   );
 }
